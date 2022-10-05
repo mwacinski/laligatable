d2119c069ad8fee88bbbbc21ee489a38f27570c9
Michał Waciński <mrfreezy48@gmail.com>
2022-10-05 18:06:13 +0200

    Few changes and GCloud connection

--- dags/laligadag.py
+++ dags/laligadag.py
@@ -1,8 +1,13 @@
 from datetime import datetime, date, timedelta
 from scripts import script1
 from airflow import DAG
+import os
 from airflow.operators.python import PythonOperator
 
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+path_to_data = '/opt/airflow/data/'
+today = str(date.today())
 default_args = {
     'owner': 'airflow',
     'retry': 5,
@@ -20,8 +25,8 @@ with DAG(
         dag=dag,
         task_id='export_table',
         op_kwargs={
-            'today': str(date.today()),
-            'path':'/opt/airflow/data/'
+            'today': today,
+            'path': path_to_data
         },
         python_callable=script1.export_table
     )
@@ -30,11 +35,20 @@ with DAG(
         dag=dag,
         task_id='export_new_table',
         op_kwargs={
-            'today': str(date.today()),
-            'path': '/opt/airflow/data/'
+            'today': today,
+            'path': path_to_data
         },
         python_callable=script1.export_new_table
     )
 
+    local_to_gcs = PythonOperator(
+        task_id="local_to_gcs",
+        python_callable=script1.upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/{today}T.csv",
+            "local_file": f"{path_to_data}{today}T.csv",
+        },
+    )
 
-export_table >> export_new_table
+export_table >> export_new_table >> local_to_gcs
